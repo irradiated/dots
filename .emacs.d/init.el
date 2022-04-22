67442f44bcab58b5bf5971545afa218aa295545e
irradiated <dakota.flath@gmail.com>
2022-04-22 01:55:09 -0700

    added emacs dracula stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,18 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'dracula t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#6272a4" "#bd93f9" "#8be9fd" "#f8f8f2"])
+ '(custom-safe-themes
+   (quote
+    ("937e7d9ab222b1062469b250e7f086b1927d78e0556c33815c70f21e67ee1419" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
